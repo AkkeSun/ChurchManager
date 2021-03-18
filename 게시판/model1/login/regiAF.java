af0b68ec3d1dcf3847ac2c8372769ea147235bf9
sun <78401555+AkkeSun@users.noreply.github.com>
2021-03-18 18:59:41 +0900

    Create regiAF.java

---
+++ 게시판/model1/login/regiAF.java
@@ -0,0 +1,59 @@
+<%@page import="dto.MemberDto"%>
+<%@page import="dao.MemberDao"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%
+request.setCharacterEncoding("utf-8");
+%>    
+    
+
+<!-- DB에 데이터를 추가 -->
+<%
+String id = request.getParameter("id");
+String pwd = request.getParameter("pwd");
+String name = request.getParameter("name");
+String email = request.getParameter("email"); 
+System.out.println(id + " " + pwd + " " + name + " " + email);
+%>
+
+
+<!-- 이동 -->
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>regiAf.jsp</title>
+
+<!-- Sweet Alert -->
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+</head>
+<body>
+
+<%
+MemberDao dao = MemberDao.getInstance();
+
+MemberDto dto = new MemberDto(id, pwd, name, email, 0);
+boolean isS = dao.addMember(dto);
+if(isS){
+	%>
+	<script type="text/javascript">
+	location.href = "login.jsp";
+	alert("성공적으로 가입되었습니다");
+	</script>
+	<%
+}else{
+	%>
+	<script type="text/javascript">
+	alert("다시 기입해 주십시오");
+	location.href = "regi.jsp";
+	</script>
+	<%
+}
+
+
+%>
+
+
+
+</body>
+</html>
